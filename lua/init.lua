c63791f957d4beb1e9ccd434869a568e354a99f7
Liu Qing <winglq@gmail.com>
2023-02-06 14:37:39 +0800

    add signature help
    Signed-off-by: Liu Qing <winglq@gmail.com>

--- lua/init.lua
+++ lua/init.lua
@@ -63,6 +63,7 @@ cmp.setup {
   sources = {
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
+    { name = 'nvim_lsp_signature_help' },
   },
 }
 
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -8,6 +8,7 @@ require('packer').startup(function()
   use 'fatih/vim-go'
   use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
   use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
+  use 'hrsh7th/cmp-nvim-lsp-signature-help'
   use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
   use 'L3MON4D3/LuaSnip' -- Snippets plugin
 end)
